616c974767904f06f38e35bbbe4c825ac5788084
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-12-16 22:36:53 +0000

    Fixed Resolution helper
    - Fied resolution helper

--- Utils/Resolution.gd
+++ Utils/Resolution.gd
@@ -2,11 +2,11 @@
 extends Node
 
 func GetWidth():
-	return OS.get_screen_size().x
+	return OS.get_window_size().x
 
 func GetHeight():
-	return OS.get_screen_size().y
+	return OS.get_window_size().y
 
 func GetResolution():
-	return OS.get_screen_size()
+	return OS.get_window_size()
 
