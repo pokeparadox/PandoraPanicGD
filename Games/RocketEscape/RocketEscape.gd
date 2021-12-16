1ec996ad4f908b59aec00515b9ba1b47f0049475
Kevin Winfield-Pantoja <pokeparadox@gmail.com>
2021-12-16 22:38:25 +0000

    Fixed clear colour
    - Fixed clear colour.

--- Games/RocketEscape/RocketEscape.gd
+++ Games/RocketEscape/RocketEscape.gd
@@ -1,6 +1,7 @@
 extends MiniGame
 
 func _ready():
+	VisualServer.set_default_clear_color(Color(0,0,0,1))
 	._ready()
 	self.Description = "Land the rocket on the landing Pad!"
 	# Setup connection of fuel bar and rocket
